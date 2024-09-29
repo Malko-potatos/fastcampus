e212c03b919fa95e5fa5177f372e8c9b573ccd30
Malko Poatatos <131747630+Malko-potatos@users.noreply.github.com>
2024-09-29 17:29:13 +0900

    initial commit

--- master/fast_toon/lib/main.dart
+++ master/fast_toon/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:fast_toon/core/router/app_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+void main() async {
   runApp(const ProviderScope(child: MyApp()));
 }
 
